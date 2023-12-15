cd5743632a1a28cdf62b006149c978235c333936
RasikaTikekar <rasika.tikekar13@gmail.com>
2023-12-15 14:19:30 +0530

    api changes

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-// Route to handle date requests
 app.get('/api/:date?', (req, res) => {
-  const { date } = req.params;
+    let dateString = req.params.date;
+  
+    if (!dateString) {
+        // If the date parameter is empty, use the current time
+        dateString = new Date().toUTCString();
+    }
 
-  // If date is not provided, use the current time
-  const inputDate = date ? new Date(date) : new Date();
+    const date = new Date(dateString);
 
-  if (isNaN(inputDate.getTime())) {
-    // Invalid date
-    return res.json({ error: 'Invalid Date' });
-  }
+    if (isNaN(date.getTime())) {
+        return res.json({ error: "Invalid Date" });
+    }
 
-  // Valid date
-  const unixTimestamp = inputDate.getTime();
-  const utcString = inputDate.toUTCString();
-
-  res.json({ unix: unixTimestamp, utc: utcString });
+    res.json({ unix: date.getTime(), utc: date.toUTCString() });
 });
 
-// Start the server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
