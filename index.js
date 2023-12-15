14c5c202c98d7a831a9ebd98b06ba43fc0b64682
RasikaTikekar <rasika.tikekar13@gmail.com>
2023-12-15 14:32:42 +0530

    api changes

--- index.js
+++ index.js
@@ -1,24 +1,52 @@
-const express = require('express');
+// const express = require('express');
+// const app = express();
+// const PORT = process.env.PORT || 3000;
+
+// app.get('/api/:date?', (req, res) => {
+//     let dateString = req.params.date;
+
+//     if (!dateString) {
+//         // If the date parameter is empty, use the current time
+//         dateString = new Date().toUTCString();
+//     }
+
+//     const date = new Date(dateString);
+
+//     if (isNaN(date.getTime())) {
+//         return res.json({ error: "Invalid Date" });
+//     }
+
+//     res.json({ unix: date.getTime(), utc: date.toUTCString() });
+// });
+
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
+
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/api/:date?', (req, res) => {
-    let dateString = req.params.date;
-  
-    if (!dateString) {
-        // If the date parameter is empty, use the current time
-        dateString = new Date().toUTCString();
-    }
+app.get("/api/:date?", (req, res) => {
+  let dateString = req.params.date;
 
-    const date = new Date(dateString);
+  if (!dateString) {
+    // If the date parameter is empty, use the current time
+    dateString = new Date().toUTCString();
+  }
 
-    if (isNaN(date.getTime())) {
-        return res.json({ error: "Invalid Date" });
-    }
+  // Check if the date parameter is a valid number
+  const isNumber = /^\d+$/.test(dateString);
 
-    res.json({ unix: date.getTime(), utc: date.toUTCString() });
+  const date = isNumber ? new Date(parseInt(dateString)) : new Date(dateString);
+
+  if (isNaN(date.getTime())) {
+    return res.json({ error: "Invalid Date" });
+  }
+
+  res.json({ unix: date.getTime(), utc: date.toUTCString() });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
